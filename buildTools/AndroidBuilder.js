92ae23467bc7bce5c8c0c4054ffbb0c9e9e21221
gormat <gormatevosyan14@gmail.com>
2016-10-19 18:51:12 +0400

    update

--- buildTools/AndroidBuilder.js
+++ buildTools/AndroidBuilder.js
@@ -44,43 +44,58 @@ class AndroidBuilder extends Builder {
         });
     }
 
-    setupProject(cb) {
-        const project = this.project;
-        if (project.canceled)
-            return cb('cancelled');
-
-        const gradleFilePath = path.join(project.projectPath, 'app', 'build.gradle');
-        fs.readFile(gradleFilePath, 'utf-8', (err, content) => {
-            if (err) {
-                return cb(err);
-            }
-
-            content = content.replace('RELEASE_STORE_FILE', project.keyStorePath);
-            content = content.replace('RELEASE_STORE_PASSWORD', project.android.keyStore.password);
-            content = content.replace('RELEASE_KEY_ALIAS', project.android.keyStore.alias);
-            content = content.replace('RELEASE_KEY_PASSWORD', project.android.keyStore.aliasPassword);
-            content = content.replace('REPLACE_PACKAGE', project.android.package);
-            content = content.replace('REPLACE_VERSION', project.version);
-            content = content.replace('REPLACE_URL', project.url);
-            content = content.replace('REPLACE_NAME', project.appName);
-
-            fs.writeFile(gradleFilePath, content, 'utf-8', (err) => {
-                if (err) {
-                    return cb(err);
-                }
-
-                console.log('gradle change success');
-                return cb();
-            })
-        });
-    }
+    // setupProject(cb) {
+    //     const project = this.project;
+    //     if (project.canceled)
+    //         return cb('cancelled');
+    //
+    //     const gradleFilePath = path.join(project.projectPath, 'app', 'build.gradle');
+    //     fs.readFile(gradleFilePath, 'utf-8', (err, content) => {
+    //         if (err) {
+    //             return cb(err);
+    //         }
+    //
+    //         content = content.replace('RELEASE_STORE_FILE', project.keyStorePath);
+    //         content = content.replace('RELEASE_STORE_PASSWORD', project.android.keyStore.password);
+    //         content = content.replace('RELEASE_KEY_ALIAS', project.android.keyStore.alias);
+    //         content = content.replace('RELEASE_KEY_PASSWORD', project.android.keyStore.aliasPassword);
+    //         content = content.replace('REPLACE_PACKAGE', project.android.package);
+    //         content = content.replace('REPLACE_VERSION', project.version);
+    //         content = content.replace('REPLACE_URL', project.url);
+    //         content = content.replace('REPLACE_NAME', project.appName);
+    //
+    //         fs.writeFile(gradleFilePath, content, 'utf-8', (err) => {
+    //             if (err) {
+    //                 return cb(err);
+    //             }
+    //
+    //             console.log('gradle change success');
+    //             return cb();
+    //         })
+    //     });
+    // }
 
     build(cb) {
         const project = this.project;
         if (project.canceled)
             return cb('cancelled');
 
-        child_process.exec('./gradlew assembleRodinRelease', {cwd: project.projectPath}, (err, stdout, stderr) => {
+        console.log('keysotrepath', project.keyStorePath);
+        const cmdParams = [
+            `-PRReleaseStoreFile="${project.keyStorePath}"`,
+            `-PRReleaseStorePassword="${project.android.keyStore.password}"`,
+            `-PRReleaseStoreKeyAlias="${project.android.keyStore.alias}"`,
+            `-PRReleaseStoreKeyPassword="${project.android.keyStore.aliasPassword}"`,
+            `-PRApplicationID="${project.android.package}"`,
+            `-PRVersionName="${project.version}"`,
+            `-PRVersionCode=${1}`,
+            `-PRURL="${project.url}"`,
+            `-PRName="${project.appName}"`
+        ];
+
+        const cmd = `./gradlew assembleRodinRelease ${cmdParams.join(" ")}`;
+
+        child_process.exec(cmd, {cwd: project.projectPath}, (err, stdout, stderr) => {
             if (stdout.match(new RegExp('BUILD SUCCESSFUL'))) {
                 project.built = 'success';
                 project.binaryPath = path.join(project.projectPath, configs.builder.buildDir, 'app-rodin-release.apk');
