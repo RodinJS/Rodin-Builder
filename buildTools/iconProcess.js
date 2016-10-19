ff15a4f009937fcae46e1b938ae73089ffd65b41
gormat <gormatevosyan14@gmail.com>
2016-10-19 18:06:07 +0400

    update

--- buildTools/iconProcess.js
+++ buildTools/iconProcess.js
@@ -22,113 +22,17 @@ let resizeImage = (inputPath, outputPath, x, y, cb) => {
 module.exports = project => cb => {
 
     const images = Object.keys(project.files).filter(i => i.indexOf('icon') !== -1);
+    const image = project.files[images[0]];
 
-    if (images.length === 0) {
-        return cb();
-    }
-
-    const imagesMap = {
-        M: "",
-        H: "",
-        XH: "",
-        XXH: "",
-        XXXH: ""
-    };
-
-    for(let i = 0; i < images.length; i ++) {
-        let size = images[i].split('-')[1].toUpperCase();
-        imagesMap[size] = images[i];
-    }
-
-    let sizes = Object.keys(imagesMap);
-    for(let i = 0; i < sizes.length; i ++) {
-        let j = 0;
-        while(imagesMap[sizes[i]] === "" && j < sizes.length) {
-            let tmp = i + j;
-            if(tmp < sizes.length && imagesMap[sizes[tmp]] !== "") {
-                imagesMap[sizes[i]] = imagesMap[sizes[tmp]];
-            }
-
-            tmp = i - j;
-            if(tmp >= 0 && imagesMap[sizes[tmp]] !== "") {
-                imagesMap[sizes[i]] = imagesMap[sizes[tmp]];
-            }
-            j ++;
-        }
-    }
-
-    async.series(
-        [
-            /***
-             * XXXH high image
-             */
-            done => {
-                const size = configs.builder.imageSizes.XXXH;
-                const image = project.files[imagesMap.XXXH][0];
-                resizeImage(image.path, path.join(project.projectPath, size.path), size.x, size.y, err => {
-                    if(!err) {
-                        console.log("icon success: xxxh");
-                    }
-                    return done(err);
-                });
-            },
-
-            /**
-             * XH high image
-             */
-            done => {
-                const size = configs.builder.imageSizes.XXH;
-                const image = project.files[imagesMap.XXH][0];
-                resizeImage(image.path, path.join(project.projectPath, size.path), size.x, size.y, err => {
-                    if(!err) {
-                        console.log("icon success: xxh");
-                    }
-                    return done(err);
-                });
-            },
-
-            /**
-             * extra high image
-             */
-            done => {
-                const size = configs.builder.imageSizes.XH;
-                const image = project.files[imagesMap.XH][0];
-                resizeImage(image.path, path.join(project.projectPath, size.path), size.x, size.y, err => {
-                    if(!err) {
-                        console.log("icon success: xh");
-                    }
-                    return done(err);
-                });
-            },
-
-            /**
-             * high image
-             */
-            done => {
-                const size = configs.builder.imageSizes.H;
-                const image = project.files[imagesMap.H][0];
-                resizeImage(image.path, path.join(project.projectPath, size.path), size.x, size.y, err => {
-                    if(!err) {
-                        console.log("icon success: h");
-                    }
-                    return done(err);
-                });
-            },
-
-            /**
-             * medium image
-             */
-            done => {
-                const size = configs.builder.imageSizes.M;
-                const image = project.files[imagesMap.M][0];
-                resizeImage(image.path, path.join(project.projectPath, size.path), size.x, size.y, err => {
-                    if(!err) {
-                        console.log("icon success: m");
-                    }
-                    return done(err);
-                });
-            }
-        ],
+    async.mapLimit(configs.builder.imageSizes, 10,
+        (imageSize, innerCb) => {
+            resizeImage(image.path, path.join(project.projectPath, imageSize.path), imageSize.width, imageSize.height, err => {
+                if (!err) {
+                    console.log("icon success");
+                }
+                return innerCb(err);
+            })
+        },
         err => {
             if (err) {
                 return cb(err);
@@ -136,5 +40,26 @@ module.exports = project => cb => {
 
             return cb();
         }
-    )
+    );
+
+    // async.series(
+    //     [
+    //         done => {
+    //             const size = configs.builder.imageSizes.XXXH;
+    //             resizeImage(image.path, path.join(project.projectPath, size.path), size.x, size.y, err => {
+    //                 if(!err) {
+    //                     console.log("icon success: xxxh");
+    //                 }
+    //                 return done(err);
+    //             });
+    //         },
+    //     ],
+    //     err => {
+    //         if (err) {
+    //             return cb(err);
+    //         }
+    //
+    //         return cb();
+    //     }
+    // )
 };
--- config/buildConfigs/ios.js
+++ config/buildConfigs/ios.js
@@ -10,34 +10,121 @@ module.exports.uuidRegExp = new RegExp(/\<key\>UUID\<\/key\>\n[\t\s]+\<string>([
 module.exports.uuNameRegExp = new RegExp(/\<key\>Name\<\/key\>\n[\t\s]+\<string>([^\<]+)\<\/string\>/, 'gi');
 module.exports.uuDevelopmentTeamRegExp = new RegExp(/\<key\>com\.apple\.developer\.team\-identifier\<\/key\>\n[\t\s]+\<string>([^\<]+)\<\/string\>/);
 
-module.exports.imageSizes = {
-    M: {
-        x: 20,
-        y: 20,
-        path: "Rodin/Assets.xcassets/AppIcon.appiconset/m.png"
-    },
-    H: {
-        x: 29,
-        y: 29,
-        path: "Rodin/Assets.xcassets/AppIcon.appiconset/h.png"
-    },
-    XH: {
-        x: 40,
-        y: 40,
-        path: "Rodin/Assets.xcassets/AppIcon.appiconset/xh.png"
-    },
-    XXH: {
-        x: 60,
-        y: 60,
-        path: "Rodin/Assets.xcassets/AppIcon.appiconset/xxh.png"
-    },
-    XXXH: {
-        x: 83.5,
-        y: 83.5,
-        path: "Rodin/Assets.xcassets/AppIcon.appiconset/xxxh.png"
-    }
-};
+// module.exports.imageSizes = {
+//     M: {
+//         x: 20,
+//         y: 20,
+//         path: "Rodin/Assets.xcassets/AppIcon.appiconset/m.png"
+//     },
+//     H: {
+//         x: 29,
+//         y: 29,
+//         path: "Rodin/Assets.xcassets/AppIcon.appiconset/h.png"
+//     },
+//     XH: {
+//         x: 40,
+//         y: 40,
+//         path: "Rodin/Assets.xcassets/AppIcon.appiconset/xh.png"
+//     },
+//     XXH: {
+//         x: 60,
+//         y: 60,
+//         path: "Rodin/Assets.xcassets/AppIcon.appiconset/xxh.png"
+//     },
+//     XXXH: {
+//         x: 83.5,
+//         y: 83.5,
+//         path: "Rodin/Assets.xcassets/AppIcon.appiconset/xxxh.png"
+//     }
+// };
 
+module.exports.imageSizes = [
+    {
+        width: 40,
+        height: 40,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_20_2x.png"
+    },
+    {
+        width: 60,
+        height: 60,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_20_3x.png"
+    },
+    {
+        width: 58,
+        height: 58,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_29_2x.png"
+    },
+    {
+        width: 87,
+        height: 87,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_29_3x.png"
+    },
+    {
+        width: 80,
+        height: 80,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_40_2x.png"
+    },
+    {
+        width: 120,
+        height: 120,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_40_3x.png"
+    },
+    {
+        width: 120,
+        height: 120,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_60_2x.png"
+    },
+    {
+        width: 180,
+        height: 180,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/iphone_60_3x.png"
+    },
+    {
+        width: 20,
+        height: 20,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_20_1x.png"
+    },
+    {
+        width: 40,
+        height: 40,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_20_2x.png"
+    },
+    {
+        width: 29,
+        height: 29,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_29_1x.png"
+    },
+    {
+        width: 58,
+        height: 58,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_29_2x.png"
+    },
+    {
+        width: 40,
+        height: 40,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_40_1x.png"
+    },
+    {
+        width: 80,
+        height: 80,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_40_2x.png"
+    },
+    {
+        width: 76,
+        height: 76,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_76_1x.png"
+    },
+    {
+        width: 152,
+        height: 152,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_76_2x.png"
+    },
+    {
+        width: 147,
+        height: 147,
+        path: "Rodin/Assets.xcassets/AppIcon.appiconset/ipad_83_5_2x.png"
+    }
+];
 module.exports.cron = {
     binSender: "* * * * *"
 };
