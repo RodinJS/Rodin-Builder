a48ffd1e658df7e604c2b7c735d1c9f21c6eb981
gormat <gormatevosyan14@gmail.com>
2016-10-22 00:32:52 +0400

    update

--- buildTools/iconProcess.js
+++ buildTools/iconProcess.js
@@ -21,12 +21,13 @@ let resizeImage = (inputPath, outputPath, x, y, cb) => {
 
 module.exports = project => cb => {
 
-    const images = Object.keys(project.files).filter(i => i.indexOf('icon') !== -1);
-    const image = project.files[images[0]][0];
 
-    if(configs.builder.imageSizes.length === 0 ) {
+    if (configs.builder.imageSizes.length === 0) {
         return cb();
     }
+
+    const images = Object.keys(project.files).filter(i => i.indexOf('icon') !== -1);
+    const image = project.files[images[0]][0];
 
     async.mapLimit(configs.builder.imageSizes, 10,
         (imageSize, innerCb) => {
